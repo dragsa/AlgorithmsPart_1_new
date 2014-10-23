3d25bc8d39a672bd4d138a15db5ceb2749ac5317
dragsa <to.gnatuk@gmail.com>
2014-10-24 01:35:50 +0300

    Fast started

--- src/Brute.java
+++ src/Brute.java
@@ -21,19 +21,15 @@ public class Brute {
         In in = new In(filename);
         int N = in.readInt();
         Point[] point = new Point[N];
-        boolean[] marker = new boolean[N];
+        StdDraw.setPenRadius(0.01);
         for (int i = 0; i < point.length; i++) {
             point[i] = new Point(in.readInt(), in.readInt());
+            point[i].draw();
         }
-//        for (int i = 0; i < point.length; i++) {
-//            System.out.println(point[i]);
-//        }
+
+        StdDraw.setPenRadius(0.005);
 
         for (int i = 0; i < point.length; i++) {
-            if (!marker[i]) {
-                StdDraw.setPenRadius(0.01);
-                point[i].draw();
-            }
             for (int j = i + 1; j < point.length; j++) {
                 for (int k = j + 1; k < point.length; k++) {
                     for (int l = k + 1; l < point.length; l++) {
@@ -42,7 +38,7 @@ public class Brute {
                             Point[] array = new Point[]{point[i], point[j], point[k], point[l]};
                             Arrays.sort(array);
                             System.out.println(array[0] + " -> " + array[1] + " -> " + array[2] + " ->" + array[3]);
-                            StdDraw.setPenRadius(0.005);
+
                             array[0].drawTo(array[3]);
                         }
                     }
--- src/Fast.java
+++ src/Fast.java
@@ -1,14 +1,62 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author dragsa
  */
 public class Fast {
-      public static void main(String[] args) {
-      
-      }
+
+    public static void main(String[] args) {
+
+        StdDraw.setXscale(0, 20);
+        StdDraw.setYscale(0, 20);
+        StdDraw.show(0);
+
+        String filename = args[0];
+        In in = new In(filename);
+        int N = in.readInt();
+        Point[] point = new Point[N];
+        StdDraw.setPenRadius(0.01);
+        for (int i = 0; i < point.length; i++) {
+            point[i] = new Point(in.readInt(), in.readInt());
+            point[i].draw();
+        }
+
+        StdDraw.show(0);
+        StdDraw.setPenRadius(0.005);
+
+        for (int i = 0; i < point.length; i++) {
+            Arrays.sort(point, point[i].SLOPE_ORDER);
+            Point[] pointList = new Point[2];
+            pointList[0] = point[0];
+            pointList[1] = point[1];
+            int segmentCounter = 1;
+            for (int j = 1; j < point.length; j++) {
+                if (j != point.length - 1) {
+                    if (point[0].slopeTo(point[j]) == point[0].slopeTo(point[j + 1])) {
+                        pointList = Arrays.copyOf(pointList, pointList.length + 1);
+                        pointList[pointList.length - 1] = point[j + 1];
+                        segmentCounter++;
+                    } else if (segmentCounter >= 3) {
+                        Arrays.sort(pointList);
+                        point[0].drawTo(pointList[pointList.length - 1]);
+                        segmentCounter = 1;
+                        StdDraw.show(0);
+                    }
+                } else if (segmentCounter >= 3) {
+                    Arrays.sort(pointList);
+                    point[0].drawTo(pointList[pointList.length - 1]);
+                    segmentCounter = 1;
+                    StdDraw.show(0);
+                }
+            }
+        }
+        StdDraw.show(0);
+    }
 }
