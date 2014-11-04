c8570aad2ac7a16d1569a72d92d5050b3255ee8e
dragsa <to.gnatuk@gmail.com>
2014-11-05 01:12:03 +0200

    Puzzle some code added

--- src/Board.java
+++ src/Board.java
@@ -1,21 +1,37 @@
+
+import java.util.Arrays;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author dragsa
  */
 public class Board {
 
+    private int[][] tiles;
+    private int N;
+
     public Board(int[][] blocks) {
+        tiles = new int[blocks.length][blocks.length];
+        for (int i = 0; i < blocks.length; i++) {
+            tiles[i] = Arrays.copyOf(blocks[i], blocks.length);
+        }
+//        tiles = blocks.clone();
+        N = tiles.length * tiles.length;
     }
     // construct a board from an N-by-N array of blocks
     // (where blocks[i][j] = block in row i, column j)
 
+    private int flatter(final int i, final int j) {
+        int flatCoordinate = (i + j * tiles.length);
+        return flatCoordinate;
+    }
+
     public int dimension() {
-        return 0;
+        return N;
     }
     // board dimension N
 
@@ -50,7 +66,25 @@ public class Board {
     // all neighboring boards
 
     public String toString() {
-        return null;
+        StringBuilder s = new StringBuilder();
+        s.append(tiles.length + "\n");
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles.length; j++) {
+                s.append(String.format("%2d ", tiles[i][j]));
+            }
+            s.append("\n");
+        }
+        //       flat structure print
+        return s.toString();
+    }
+    
+    public void flaterResult() {
+        for (int i = 0; i < tiles.length; i++) {
+            for (int j = 0; j < tiles.length; j++) {
+                System.out.print(flatter(j, i) + " ");
+            }
+            System.out.println("");
+        }
     }
     // string representation of this board (in the output format specified below)
 
