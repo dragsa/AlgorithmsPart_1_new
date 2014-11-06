fc8c0710d83e8ff9014969400c0551fefbcc985e
agnatyuk <to.gnatuk@gmail.com>
2014-11-06 19:59:01 +0200

    Board neigh started

---
+++ test/JBoardTester.java
@@ -0,0 +1,46 @@
+
+import java.io.File;
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agnatyuk
+ */
+public class JBoardTester {
+    
+    static Board boardInstance;
+
+    //execute for each test, before executing test
+    @BeforeClass
+    public static void before() {
+         In in = new In(new File("C:\\Users\\agnatyuk\\Documents\\NetBeansProjects\\AlgorithmsPart_1_new\\src\\puzzle10.txt"));
+            int N = in.readInt();
+            int[][] tiles = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    tiles[i][j] = in.readInt();
+                }
+            }
+
+            // solve the slider puzzle
+            boardInstance = new Board(tiles);
+    }
+
+    @Test
+    public void testHamming() {
+        assertEquals(boardInstance.hamming(), 10);
+    }
+    
+    @Test
+    public void testManhatten() {
+        assertEquals(boardInstance.manhattan(), 10);
+    }
+}
